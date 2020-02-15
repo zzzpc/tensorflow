39c5ddf01d2b3e7f42ff6ac5b1fe7a2d88a14bbf
PC_zha <30690856+zzzpc@users.noreply.github.com>
2020-02-15 16:47:43 +0800

    Update PCA_MNIST_diffPrivacy.py

--- PCA_MNIST_diffPrivacy.py
+++ PCA_MNIST_diffPrivacy.py
@@ -144,9 +144,17 @@ if __name__ == "__main__":
     w2 = tf.Variable(tf.truncated_normal([h1_units,10],stddev=0.1),dtype=tf.float32)
     b2 = tf.Variable(tf.zeros([10],dtype=tf.float32))
     #定义输入变量
+
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    add_global = global_step.assign_add(1)
+    boundaries = [2,4,6,8,10]
+    learing_rates = [0.1,0.0952,0.0856,.0664, 0.0568, 0.052]
+    lr = tf.train.piecewise_constant(global_step, boundaries=boundaries, values=learing_rates)
     x = tf.placeholder(dtype=tf.float32,shape=[None,in_units])
     #定义dropout保留的节点数量
     keep_prob = tf.placeholder(dtype=tf.float32)
+    #定义学习率变量
+
     #定义前向传播过程
     h1 = tf.nn.relu(tf.add(tf.matmul(x,w1),b1))
     #使用dropout
@@ -157,25 +165,27 @@ if __name__ == "__main__":
     y_ = tf.placeholder(dtype=tf.float32,shape=[None,10])
     #定义损失函数
     loss_func = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y),reduction_indices=[1]))
-    train_step = tf.train.AdagradOptimizer(0.1).minimize(loss_func)
+    train_step = tf.train.AdagradOptimizer(lr).minimize(loss_func)
 
     correct_pred = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
     #初始化变量
     tf.global_variables_initializer().run()
-    for i in range(10000):
-        batch_xs,batch_ys = train.next_batch(100)
-        train_step.run({x:batch_xs,y_:batch_ys,keep_prob:0.75})
-        if i%100==0:
-            print("train accuracy:",
-                  accuracy.eval({x: batch_xs, y_: batch_ys, keep_prob: 1.0}))
-    #计算准确率
+
+    epoch=20
+    batch=100
+    for step  in range(epoch):
+        ra=sess.run(lr)
+        for i in range(600):
+            batch_xs, batch_ys = train.next_batch(100)
+            train_step.run({x: batch_xs, y_: batch_ys, keep_prob: 0.75})
+            if i % 100 == 0:
+                print("train accuracy :",accuracy.eval({x: batch_xs, y_: batch_ys, keep_prob: 1.0}),"当前学习率为%f:" % ra)
+        # 计算准确率
+        test_acc=accuracy.eval({x:x_test_reduction, y_: mnist.test.labels, keep_prob: 1.0},session=sess)
+        g=sess.run(add_global)
+        print("第%d个epoch训练完毕 测试精确度未%.2f ，马上进入第%d个epoch的训练" %(step,test_acc,g) )
 
     #训练集上的准确率
 
     print("test accuracy:",accuracy.eval({x:x_test_reduction,y_:mnist.test.labels,keep_prob:1.0}))
-    '''
-    train accuracy: 0.9917455
-    test accuracy: 0.9804
-    '''
-
