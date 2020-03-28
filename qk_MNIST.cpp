acc193fda28334a1bd2924e9745c725e7ece2a8e
PC_zha <30690856+zzzpc@users.noreply.github.com>
2020-03-28 18:41:38 +0800

    Update qk_MNIST.cpp

--- qk_MNIST.cpp
+++ qk_MNIST.cpp
@@ -57,6 +57,24 @@ typedef struct{
 }Ent;
 Ent NDB[N];
 int Pos[6272][2] = { 0 };
+double Gen[10000][784];
+double posbility[8][2] = { 0 };
+
+double  diff(int i){                  //计算与原串不同的概率
+	double Ndiff = 0;
+	double Nsame = 0;
+	for (int j = 1; j <= 3; j++){
+		Ndiff += j*p[j] * q[i];
+	}
+	for (int j = 1; j <= 3; j++){
+		Nsame += ((3 - j)*p[j]) / 8;
+	}
+
+	double Pdiff = 0;
+	Pdiff = Ndiff / (Ndiff + Nsame);
+	return Pdiff;
+
+}
 void init()
 {
 	m = 6272;
@@ -67,7 +85,7 @@ void init()
 	p[3] = 1 - p[1] - p[2];
 	cn = int(m*r + 0.5);
 
-	
+
 	q[0] = 0.3;
 	q[1] = 0.1;
 	q[2] = 0.1;
@@ -76,6 +94,12 @@ void init()
 	q[5] = 0.1;
 	q[6] = 0.1;
 	q[7] = 0.1;
+
+
+	for (int i = 0; i < 8; i++){
+		posbility[i][0] = diff(i);
+		posbility[i][1] = 1 - posbility[i][0];
+	}
 }
 
 int rand1(int n)
@@ -90,17 +114,17 @@ double rand1()
 }
 
 int  generateRandomNumbers(double l)	//取了三个随机位 
-{   
-	
-	if (l < q[7]) return 8; 
-	else if (l < q[7] + q[6]) return 7;
-	else if (l < q[7] + q[6] + q[5])return 6;
-	else if (l < q[7] + q[6] + q[5] + q[4])return 5;
-	else if (l < q[7] + q[6] + q[5] + q[4] + q[3])return 4;
-	else if (l < q[7] + q[6] + q[5] + q[4] + q[3] + q[2])return 3;
-	else if (l < q[7] + q[6] + q[5] + q[4] + q[3] + q[2] + q[1])return 2;
-	else return 1;
-	
+{
+
+	if (l < q[7]) return 7;
+	else if (l < q[7] + q[6]) return 6;
+	else if (l < q[7] + q[6] + q[5])return 5;
+	else if (l < q[7] + q[6] + q[5] + q[4])return 4;
+	else if (l < q[7] + q[6] + q[5] + q[4] + q[3])return 3;
+	else if (l < q[7] + q[6] + q[5] + q[4] + q[3] + q[2])return 2;
+	else if (l < q[7] + q[6] + q[5] + q[4] + q[3] + q[2] + q[1])return 1;
+	else return 0;
+
 }
 
 void  geneRandombit(Ent &v, int l){
@@ -138,40 +162,40 @@ void f(char s[])		//生成s[]的负数据库
 	double t;
 	double u;
 	n = int(m*r + 0.5);
-	
-	
+
+
 	//四舍五入 
 	cn = 0;
 	//clock_t start,end; // typedef long clock_t
 	//    start = clock();
 	do
 	{
-					//取3个随机位 
+		//取3个随机位 
 		t = rand1();				//生成0~1之间的小数 
 		if (t<p[1])                 //生成类型一
-		{	
+		{
 			int diff1 = 0;
 			int same1 = 0;
 			int same2 = 0;
 			int attr1 = 0;
-			u = rand1(); 
-			diff1=generateRandomNumbers(u);  //通过属性q决定属性的哪一位与原始位不同
-			attr1 = rand1(m/8);
-			v.p[0] = diff1+attr1*8-1;      //生成的反转位
+			u = rand1();
+			diff1 = generateRandomNumbers(u);  //通过属性q决定属性的哪一位与原始位不同
+			attr1 = rand1(m / 8);
+			v.p[0] = diff1 + attr1 * 8 ;      //生成的反转位
 			v.c[0] = '1' + '0' - s[v.p[0]];
-			
+
 			same1 = rand1(8);
 			while (same1 == diff1){
 				same1 = rand1(8);
 			}
-			v.p[1] = same1 + attr1 * 8-1;
+			v.p[1] = same1 + attr1 * 8 ;
 			v.c[1] = s[v.p[1]];
 
 			same2 = rand1(8);
-			while (same2 == diff1 || same2== same1){
+			while (same2 == diff1 || same2 == same1){
 				same2 = rand1(8);
 			}
-			v.p[2] = same2 + attr1 * 8-1;
+			v.p[2] = same2 + attr1 * 8;
 			v.c[2] = s[v.p[2]];
 
 
@@ -182,25 +206,25 @@ void f(char s[])		//生成s[]的负数据库
 			int diff2 = 0;
 			int same1 = 0;
 			int attr = 0;
-			
+
 			diff1 = generateRandomNumbers(rand1());  //通过属性q决定属性的哪一位与原始位不同
 			diff2 = generateRandomNumbers(rand1());
-			
+
 			attr = rand1(m / 8);
-			v.p[0] = diff1 + attr * 8-1;      //生成的反转位
+			v.p[0] = diff1 + attr * 8 ;      //生成的反转位
 			v.c[0] = '1' + '0' - s[v.p[0]];
 			while (diff2 == diff1){
 				diff2 = generateRandomNumbers(rand1());
 			}
 
-			v.p[1] = diff2 + attr * 8-1;
+			v.p[1] = diff2 + attr * 8;
 			v.c[1] = '1' + '0' - s[v.p[1]];
 
 			same1 = rand1(8);
 			while (same1 == diff1 || same1 == diff2){
 				same1 = rand1(8);
 			}
-			v.p[2] = same1 + attr * 8-1;
+			v.p[2] = same1 + attr * 8;
 			v.c[2] = s[v.p[2]];
 		}
 		else
@@ -213,11 +237,13 @@ void f(char s[])		//生成s[]的负数据库
 			attr = rand1(m / 8);
 			v.p[0] = diff1 + attr * 8;      //生成的反转位
 			v.c[0] = '1' + '0' - s[v.p[0]];
+			diff2 = generateRandomNumbers(rand1());
 			while (diff2 == diff1){
 				diff2 = generateRandomNumbers(rand1());
 			}
 			v.p[1] = diff2 + attr * 8;      //生成的反转位
 			v.c[1] = '1' + '0' - s[v.p[1]];
+			diff3 = generateRandomNumbers(rand1());
 			while (diff3 == diff1 || diff3 == diff2){
 				diff3 = generateRandomNumbers(rand1());
 			}
@@ -249,21 +275,7 @@ void printNDB()				//输出确定位和确定位的值
 	outfile.close();
 }
 
-double  diff(int i){                  //计算与原串不同的概率
-	double Ndiff = 0;
-	double Nsame = 0;
-	for (int j = 1; j <= 3; j++){
-		Ndiff += j*p[j] * q[i];
-	}
-	for (int j = 1; j <=3; j++){
-		Nsame += ((3 - j)*p[j])/8;
-	}
 
-	double Pdiff = 0;
-	Pdiff = Ndiff / (Ndiff + Nsame);
-	return Pdiff;
-
-}
 
 int num_01(){
 
@@ -273,11 +285,7 @@ int num_01(){
 			else Pos[NDB[i].p[j]][1]++;
 		}
 	}
-	/*
-	for (int i = 0; i < 6273; i++){
-	cout<<i<<" "<< Pos[i][0] << " " << Pos[i][1] << endl;
-	}
-	*/
+	
 
 	return 0;
 }
@@ -288,9 +296,8 @@ double  Pr(int index, int num){       //传入的值为原始穿中第i位的索
 	double pr1 = 0;
 	double pr0 = 0;
 	int tmp = index % 8;
-	double pdiff = diff(tmp);
-	double psame = 1 - pdiff;
-	//cout <<Pos[index]<<" "<< Pos[index][0]<<" "<<Pos[index][1] << endl;
+	double pdiff = diff(tmp);    //posbility[tmp][0];
+	double psame = 1 - pdiff;     //posbility[tmp][1];
 	if (num == 1){
 		pr1 = (pow(pdiff, Pos[index][0])*pow(psame, Pos[index][1])) / (pow(pdiff, Pos[index][1])*pow(psame, Pos[index][0]) + pow(pdiff, Pos[index][0])*pow(psame, Pos[index][1]));
 		return pr1;
@@ -320,66 +327,20 @@ double  E(int index){      //计算期望值
 	//tmp = tmp / 256;
 	return tmp;
 }
-void readfile(char arry[]){
-	/*
-	ifstream myfile;
-	myfile.open("G:\\python\\.txt");
-	char tmp;
-	int count = 0;
-	char Ostring[M] = {-1};
-	while (!myfile.eof()){
-	myfile >> tmp;
-	Ostring[count] = tmp;
-	count++;
-	}
-	*/
-	int cc = 0;
-	int temp[8] = { -1 };
-	int count1 = 0;
+void readfile(int count){
+
 	int count2 = 0;
-	double final[6272];
 	for (int i = 0; i < 6272; i++){
-		if (i % 8 == 0)		final[count2++] = E(i);     //每隔8位传入原始01串的索引值生成8位01串对应0-255每位值的期望值 count2代表最后生成矩阵的元素个数
+		if (i % 8 == 0)		Gen[count][count2++] = E(i);     //每隔8位传入原始01串的索引值生成8位01串对应0-255每位值的期望值 count2代表最后生成矩阵的元素个数
 	}
-	//cout << count2;
-	ofstream   outfile;
-	outfile.open("H:\\qk_test.txt", ios::app);
-	for (int i = 0; i <784; i++){
 
-		outfile << final[i] << " ";
-	}
-	outfile << endl;
-
-	outfile.close();
 	return;
 }
 int main(){
-	/*
-	ifstream myfile;
-	string tmp;
-	char index[100] = { 0 };
-	char NDb[N];
-	int count = 0;
-	init();
-	time_t  start = time(NULL);
-	sprintf(index, "G:\\python\\zpc.txt");
-	myfile.open(index);
-	while (getline(myfile, tmp))   //按行读取,遇到换行符结束
-	{
-	count++;
-	f(strcpy(NDb, tmp.c_str()));
-	printNDB();
-	//cout<<tmp << endl;
-	}
-
-	time_t  end = time(NULL);
-	cout << "算法执行持续时间：" << difftime(end, start) << "秒" << count<<endl;
-	*/
 
 	init();
 	ifstream myfile;
 	string tmp;
-	time_t  start = time(NULL);
 	myfile.open("H:\\cc_test.txt");
 	int count = 0;
 	while (!myfile.eof())   //按行读取,遇到换行符结束
@@ -387,32 +348,36 @@ int main(){
 		myfile >> tmp;
 		if (myfile.peek() == EOF)  break;
 		(strcpy(NDb[count++], tmp.c_str()));
-		if (count == 10) break;
+		if (count == 100) break;
 
-		//cout << tmp.length();    输出图片的长度
-		//cout<<tmp << endl;
+
 	}
+	myfile.close();
 	cout << count << "张图片的01串已经生成" << endl;
 
+	time_t  start = time(NULL);
 
-	//cout << (sizeof(NDb) / sizeof(NDb[0])) << endl;
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < count; i++){
 		f(NDb[i]); //生成每张图片所对应的负数据库记录。
 		num_01();    //计算负数据库中所有记录条数中相对于原串中为位分别为0 或者1的个数
-		readfile(NDb[i]);  //生成每张求完期望之后的图片
+		readfile(i);  //生成每张求完期望之后的图片
 		for (int i = 0; i < 6272; i++){
 			for (int j = 0; j < 2; j++){
 				Pos[i][j] = 0;
 			}
 		}
+	}
+	time_t  end = time(NULL);
+	cout << "负数据库生成以及重构执行持续时间：" << difftime(end, start) << "秒" << endl;
+	ofstream   outfile;
+	outfile.open("H:\\k.txt", ios::app);
+	for (int i = 0; i < count; i++){
+		for (int j = 0; j <784; j++){
+			outfile << Gen[i][j] << " ";
+		}
+		outfile << endl;
 
 	}
-	myfile.close();
-	cout << "10000张图片的MNIST 矩阵已经生成" << endl;
-	time_t  end = time(NULL);
-	cout << "算法执行持续时间：" << difftime(end, start) << "秒" << endl;
-	time_t  start1 = time(NULL);
+	outfile.close();
 
-	time_t  end1 = time(NULL);
-	cout << "算法执行持续时间：" << difftime(end1, start1) << "秒" << endl;
 }
